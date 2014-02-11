38d03a75640104fef09230bb6dc5fb9aefaf93ab
Muhammad <Muhammad@Zohaib>
2014-02-11 06:36:55 -0800

    Zohaib Noob

---
+++ application/views/include/footer.php
@@ -0,0 +1,23 @@
+ </div>
+        <!-- /#wrapper -->
+
+        <!-- Core Scripts - Include with every page -->
+        <script src="<?php echo base_url('assets/js/jquery-1.10.2.js');?>"></script>
+        <script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
+        <script src="<?php echo base_url('assets/js/plugins/metisMenu/jquery.metisMenu.js');?>"></script>
+
+        <!-- Page-Level Plugin Scripts - Dashboard -->
+        <script src="<?php echo base_url('assets/js/plugins/morris/raphael-2.1.0.min.js');?>"></script>
+        <script src="<?php echo base_url('assets/js/plugins/morris/morris.js');?>"></script>
+
+        <!-- Mint Admin Scripts - Include with every page -->
+        <script src="<?php echo base_url('assets/js/mint-admin.js');?>"></script>
+
+        <!-- Page-Level Demo Scripts - Dashboard - Use for reference -->
+        <script src="<?php echo base_url('assets/js/demo/dashboard-demo.js');?>"></script>
+
+    </body>
+
+
+<!-- Mirrored from themes.grozav.com/mint/index.html by HTTrack Website Copier/3.x [XR&CO'2013], Mon, 10 Feb 2014 12:07:48 GMT -->
+</html>
---
+++ application/views/include/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+    
+<!-- Mirrored from themes.grozav.com/mint/index.html by HTTrack Website Copier/3.x [XR&CO'2013], Mon, 10 Feb 2014 12:07:34 GMT -->
+<head>
+
+        <title>Travly - Admin Panel</title>
+
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+        <meta name="description" content="Mint by Grozav is a flat design approach towards Admin Dashboards. Intuitive, cutting-edge, clean and easy to use and customize, as every Application UI should be. ">
+
+        <meta name="keywords" content="mint, flat, admin, dashboard, app, template, theme, grozav, admin template, admin dashboard, bootstrap, admin bootstrap, awesome" />
+
+        <link rel="author" href="http://grozav.com/"/>
+
+        <!-- Core CSS - Include with every page -->
+        <link href="<?php echo base_url('assets/css/bootstrap.css');?>" rel="stylesheet">
+        <link href="<?php echo base_url('assets/netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css');?>" rel="stylesheet">
+
+        <!-- Page-Level Plugin CSS - Dashboard -->
+        <link href="<?php echo base_url('assets/css/plugins/morris/morris-0.4.3.min.css');?>" rel="stylesheet">
+        <link href="<?php echo base_url('assets/css/plugins/timeline/timeline.css');?>" rel="stylesheet">
+
+        <!-- Mint Admin CSS - Include with every page -->
+        <link href="<?php echo base_url('assets/css/mint-admin.css');?>" rel="stylesheet">
+
+    </head>
+
+    <body>
+
+        <div id="wrapper">
+
+
+            
+       
